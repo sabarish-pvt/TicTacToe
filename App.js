6e9678f275ecab1fd0e6ea06bee6b6be59b55049
sabarish-pvt <sabarishsabarish540@gmail.com>
2024-11-19 19:01:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import TicTacToe from './Components/tictactoe/tictactoe';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
+
